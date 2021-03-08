2f200a74164684544cc653c4ba751f4aec75170b
Paul Hadchiti <pah73@georgetown.edu>
2021-03-08 13:17:07 -0500

    parsed response working

--- robo_advisor.py
+++ robo_advisor.py
@@ -1,14 +1,42 @@
 import json
 import requests
 import os
+import csv
 
 def to_usd(my_price):
     return f"${my_price:,.2f}" #> $12,000.71
 
+ticker = ""
 api_key=os.environ.get("ALPHAVANTAGE_API_KEY")
-request_url=f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
-
+request_url= "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo"
 response = requests.get(request_url)
+
+print(type(response)) # > <class 'requests.models.Response'>
+print(response.status_code) # > 200
+print(response.text) 
+
+#parse response texts from string to dictionary
 parsed_response = json.loads(response.text)
 
-tsd=parsed_response["Time Series (Daily)"]
+breakpoint()
+
+
+
+time_series_daily = parsed_response["Time Series (Daily)"]
+
+print("-------------------------")
+print("SELECTED SYMBOL: XYZ")
+print("-------------------------")
+print("REQUESTING STOCK MARKET DATA...")
+print("REQUEST AT: 2018-02-20 02:00pm")
+print("-------------------------")
+print("LATEST DAY: 2018-02-20")
+print("LATEST CLOSE: $100,000.00")
+print("RECENT HIGH: $101,000.00")
+print("RECENT LOW: $99,000.00")
+print("-------------------------")
+print("RECOMMENDATION: BUY!")
+print("RECOMMENDATION REASON: TODO")
+print("-------------------------")
+print("HAPPY INVESTING!")
+print("-------------------------")
