fb0e3f90fe8c6747bb2262b7d39b7751f1f4f707
Paul Hadchiti <pah73@georgetown.edu>
2021-03-08 13:55:33 -0500

    got latest interval working

--- robo_advisor.py
+++ robo_advisor.py
@@ -3,22 +3,28 @@ import requests
 import os
 import csv
 
+#converting numbers into price (used in previous projects)
 def to_usd(my_price):
-    return f"${my_price:,.2f}" #> $12,000.71
+    return f"${my_price:,.2f}" 
 
 ticker = ""
 api_key=os.environ.get("ALPHAVANTAGE_API_KEY")
 request_url= "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo"
 response = requests.get(request_url)
-
-print(type(response)) # > <class 'requests.models.Response'>
-print(response.status_code) # > 200
-print(response.text) 
-
 #parse response texts from string to dictionary
 parsed_response = json.loads(response.text)
+
+#finding time it was last refreshed
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
-print(last_refreshed)
+
+
+#finding latest prices
+tsd=parsed_response["Time Series (5min)"] #have been exploring with smaller intervals because I am trying to adapt this code to crypto day-trading eventually using Binance API 
+dates = list(tsd.keys())
+latest_interval = dates[0] # pulling the most recent day from list, assuming latest day is first
+
+latest_closing_price = tsd[latest_interval]["4. close"]
+
 #breakpoint()
 
 
@@ -29,10 +35,10 @@ print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print("REQUEST AT: 2018-02-20 02:00pm") #find current date / when request was made using date/time module
 print("-------------------------")
-print(f"LATEST DAY:  {last_refreshed}")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST TIME:  {last_refreshed}")
+print(f"LATEST CLOSE: {to_usd(float(latest_closing_price))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
